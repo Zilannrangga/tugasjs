02b2926195b18df41721804beccf3580a63c1656
ahmad <ahmadhudaya989@gmail.com>
2022-02-08 14:23:14 +0700

    commit pertama

---
+++ no4.js
@@ -0,0 +1,16 @@
+//no4
+let Biodata = {
+    nama: `zillan`,
+    kelas: `XI RPL 2`,
+    alamat: `ciwidey`,
+    umur: 17,
+    hardware: {
+        nama: `poco M3 pro 5G`,
+        spec: {
+        chipset: `mediatek 700 dimensity 5G`,
+        ram: `6gb`,
+        rom: `128gb`,    
+        }
+    }
+}
+console.log (Biodata.hardware);
